f128dffa926a3c68c56b8fe186c4e4694769241b
Brice Duke <bricewduke@gmail.com>
2021-04-25 23:33:17 -0500

    🎉 Initial Commit!

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from 'react';
 
-function App() {
+const App: FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="form">
+          <input type="text" placeholder="Task" />
+          <button>Add Task</button>
+        </div>
+      </div>
+      <div className="list">
+
+      </div>
     </div>
   );
 }
